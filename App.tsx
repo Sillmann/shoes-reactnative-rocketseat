d5b7f0d3f606a0587af25f4976a94bf320cd215e
ssilman <ssilman@gmail.com>
2024-03-29 17:31:12 -0300

    atualizacoes

--- App.tsx
+++ App.tsx
@@ -2,15 +2,23 @@ import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { OneSignal } from 'react-native-onesignal'
+
 import { Routes } from './src/routes';
 
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+import { tagUserEmailCreate } from './src/notifications/notificationTags';
+
+OneSignal.initialize("<API_KEY_ONESIGNAL>");
+OneSignal.Notifications.requestPermission(true);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  tagUserEmailCreate("ssilman@gmail.com");
 
   return (
     <NativeBaseProvider theme={THEME}>
